cd262834a86bfc129f8f194f82b9244b7dd3af6c
sendistephen <sendi.steve2@gmail.com>
2022-04-24 04:08:16 +0300

    Setup initial project structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+	return (
+		<div className="App">
+			<h1 className="text-gray-900 text-xl">Just Getting Started!</h1>
+		</div>
+	);
+}
+
+export default App;
